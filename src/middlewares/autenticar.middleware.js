2533dbe6cfeaadefbdc1de6cc5a0dab0345bafc0
kevin <64976960+impake45@users.noreply.github.com>
2026-09-14 18:07:41 -0500

    fix: segun lo acordado realizo commit de autenticacion con middleware sin modelos

---
+++ src/middlewares/autenticar.middleware.js
@@ -0,0 +1,24 @@
+// aca verificamos quien es el usuario para asi poder ingresar gracias a los tokens
+
+import jwt from "jsonwebtoken";
+
+// con esta funcion se verifica que halla un token valido para el ingreso y si no da el error 401 respectivamente
+function verificarToken(req, res, next) {
+  const authHeader = req.headers.authorization;
+// aca se revisa que el header existe y que tenga un formato valido sino 401
+  if (!authHeader || !authHeader.startsWith("Bearer ")) {
+    return res.status(401).json({ mensaje: "Token no proporcionado" });
+  }
+
+  const token = authHeader.split(" ")[1];
+// esto es para que no se caiga la app y verifica si la "firma" es igual a la original
+  try {
+    const payload = jwt.verify(token, process.env.JWT_SECRET);
+    req.usuario = payload; // { id, mail_user, rol }
+    next();
+  } catch (error) {
+    return res.status(401).json({ mensaje: "Token inválido o expirado" });
+  }
+}
+
+export default verificarToken;
