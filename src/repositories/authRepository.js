58dd25c6dead212b62986474ee99ee6a17f3f23e
Dioneiver Linares <dlinaresfernandez@gmail.com>
2026-09-14 18:28:32 -0500

    fix: corrigiendo la estructura de los tokens como lo pedia frontend

--- src/repositories/authRepository.js
+++ src/repositories/authRepository.js
@@ -2,7 +2,7 @@ import db from '../config/db.js';
 
 const findByEmail = async (correo) => {
     const [rows] = await db.query(
-        'SELECT u.id_user, u.mail_user, u.password_user, u.id_rol, r.name_role FROM users u INNER JOIN rol r ON u.id_rol = r.id_rol WHERE u.mail_user = ?',
+        'SELECT u.id_user, u.mail_user, u.password_user, u.id_rol, r.name_role, s.name_student FROM users u INNER JOIN rol r ON u.id_rol = r.id_rol LEFT JOIN students s ON u.id_user = s.id_user WHERE u.mail_user = ?',
         [correo]
     );
     return rows[0];
--- src/services/authService.js
+++ src/services/authService.js
@@ -5,37 +5,42 @@ import { findByEmail } from '../repositories/authRepository.js';
 
 dotenv.config();
 
+
 const login = async (data) => {
-    // 1. Buscar usuario
     const usuario = await findByEmail(data.correo);
 
     if (!usuario) {
         throw { status: 404, mensaje: 'Correo electrónico no registrado' };
     }
 
-    // 2. Comprobar contraseña
     const contrasenaValida = await bcrypt.compare(data.contrasena, usuario.password_user);
 
     if (!contrasenaValida) {
         throw { status: 401, mensaje: 'Contraseña incorrecta' };
     }
 
-    // 3. Generar token
+    const rolTraducido = usuario.name_role;
+
+    // Obtener el nombre real
+    const nombreReal = usuario.name_student || usuario.mail_user;
+
     const token = jwt.sign(
         {
             id: usuario.id_user,
             correo: usuario.mail_user,
-            rol: usuario.name_role.toLowerCase(),
-            nombre: usuario.mail_user
+            rol: rolTraducido,
+            nombre: nombreReal
         },
         process.env.JWT_SECRET,
         { expiresIn: process.env.JWT_EXPIRES_IN }
     );
 
-    // 4. Devolver información
     return {
-        token,
-        rol: usuario.name_role.toLowerCase(),
+        token: token,
+        id: usuario.id_user,
+        nombre: nombreReal,
+        correo: usuario.mail_user,
+        rol: rolTraducido,
         mensaje: 'Inicio de sesión exitoso'
     };
 };
