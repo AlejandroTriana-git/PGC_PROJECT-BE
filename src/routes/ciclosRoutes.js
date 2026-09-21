3ac86cf763177c54345a6622ea9105dd8215b83f
kevin <64976960+impake45@users.noreply.github.com>
2026-09-21 15:26:49 -0500

    feat: endpoints de ciclos - crear, editar y listar (#14, #15, #16)
     se crea endpoints de ciclos con lo que hace un crud a excepcion de eliminar y se ajusta el nombre de los archivos para que la estructura del proyecto tenga un orden

---
+++ src/routes/ciclosRoutes.js
@@ -0,0 +1,12 @@
+import { Router } from "express";
+import verificarToken from "../middlewares/auth.middleware.js";
+import autorizarRoles from "../middlewares/role.middleware.js";
+import * as cyclesController from "../controllers/cycles.controller.js";
+
+const router = Router();
+// aca se llama a los middlewares para los roles
+router.post("/", verificarToken, autorizarRoles("Coordinador"), cyclesController.crear);
+router.put("/:id", verificarToken, autorizarRoles("Coordinador"), cyclesController.editar);
+router.get("/", verificarToken, cyclesController.listar);
+
+export default router;
